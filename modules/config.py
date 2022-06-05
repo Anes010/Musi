e7d9cb9f2429d298b897986f7d0539e464b517cf
Anes010 <98156562+Anes010@users.noreply.github.com>
2022-06-05 13:00:24 +0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -30,4 +30,4 @@ UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/N_B_10")
 
 # 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (ANES) 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ . !").split())
-PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/N_B_1")
+PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/S8Y8S")
